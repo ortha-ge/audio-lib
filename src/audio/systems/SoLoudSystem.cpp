81c0524ee3503eec7f13c7dbf25378517200d59c
Niamh Cuileann <sweet.niamhy@gmail.com>
2025-09-01 22:47:43 -0400

    Added workaround for Wav destructor causing crashes. SoLoud has some major lifetime/constructor design issues.

--- src/audio/systems/SoLoudSystem.cpp
+++ src/audio/systems/SoLoudSystem.cpp
@@ -2,9 +2,9 @@ module;
 
 #include <utility>
 
+#include <entt/entt.hpp>
 #include <soloud.h>
 #include <soloud_wav.h>
-#include <entt/entt.hpp>
 
 module Audio.SoLoudSystem;
 
@@ -17,57 +17,57 @@ import Core.ResourceHandle;
 
 namespace Audio {
 
-    struct SoLoudAudioSource {
-        std::unique_ptr<SoLoud::Wav> wav{};
-    };
+	struct SoLoudAudioSource {
+		// BUG: Wav data seems to be used after shutdown, deleting causes crash.
+		SoLoud::Wav* wav{nullptr};
+	};
 
-    SoLoudSystem::SoLoudSystem(Core::EnTTRegistry& registry, Core::Scheduler& scheduler)
-        : mRegistry{ registry }
-        , mScheduler{ scheduler } {
+	SoLoudSystem::SoLoudSystem(Core::EnTTRegistry& registry, Core::Scheduler& scheduler) :
+		mRegistry{registry}, mScheduler{scheduler} {
 
-        mSoloud.init();
-        mTickHandle = mScheduler.schedule([this]() {
-            tick(mRegistry);
-        });
-    }
+		mSoloud.init();
+		mTickHandle = mScheduler.schedule([this]() { tick(mRegistry); });
+	}
 
-    SoLoudSystem::~SoLoudSystem() {
-        mScheduler.unschedule(std::move(mTickHandle));
-        mSoloud.deinit();
-    }
+	SoLoudSystem::~SoLoudSystem() {
+		mScheduler.unschedule(std::move(mTickHandle));
+		mSoloud.stopAll();
+		mSoloud.deinit();
+	}
 
-    void SoLoudSystem::tick(entt::registry& registry) {
-        auto createSoundResourceView = registry.view<const Core::RawDataResource, Audio::SoundDescriptor>(entt::exclude<Core::FileLoadRequest, SoLoudAudioSource>);
-        createSoundResourceView.each([&registry](entt::entity entity, const Core::RawDataResource& rawData) {
-            auto wavSource = std::make_unique<SoLoud::Wav>();
-            wavSource->loadMem(const_cast<unsigned char*>(rawData.rawData.data()), rawData.size, true, false);
+	void SoLoudSystem::tick(entt::registry& registry) {
+		auto createSoundResourceView = registry.view<const Core::RawDataResource, Audio::SoundDescriptor>(
+				entt::exclude<Core::FileLoadRequest, SoLoudAudioSource>);
+		createSoundResourceView.each([&registry](entt::entity entity, const Core::RawDataResource& rawData) {
+			auto& soLoudAudioSource{registry.emplace<SoLoudAudioSource>(entity)};
+			soLoudAudioSource.wav = new SoLoud::Wav();
+			soLoudAudioSource.wav->loadMem(rawData.rawData.data(), rawData.size, true, false);
+		});
 
-            registry.emplace<SoLoudAudioSource>(entity, std::move(wavSource));
-        });
+		registry.view<AudioSource, PlaySoundSourceRequest>().each(
+				[this, &registry](entt::entity entity, AudioSource& audioSource,
+								  const PlaySoundSourceRequest& request) {
+					if (audioSource.soundResource == entt::null) {
+						return;
+					}
 
-        auto playSoundSourceRequestView = registry.view<Audio::AudioSource, PlaySoundSourceRequest>();
-        playSoundSourceRequestView.each([this, &registry](entt::entity entity, Audio::AudioSource& audioSource, const PlaySoundSourceRequest& request) {
-			if (audioSource.soundResource == entt::null) {
-				return;
-			}
+					if (!registry.all_of<Core::ResourceHandle>(audioSource.soundResource)) {
+						return;
+					}
 
-			if (!registry.all_of<Core::ResourceHandle>(audioSource.soundResource)) {
-				return;
-			}
+					const auto& soundResourceHandle = registry.get<Core::ResourceHandle>(audioSource.soundResource);
+					if (soundResourceHandle.mResourceEntity == entt::null ||
+						!registry.all_of<SoLoudAudioSource>(soundResourceHandle.mResourceEntity)) {
+						return;
+					}
 
-			const auto& soundResourceHandle = registry.get<Core::ResourceHandle>(audioSource.soundResource);
-			if (soundResourceHandle.mResourceEntity == entt::null ||
-				!registry.all_of<SoLoudAudioSource>(soundResourceHandle.mResourceEntity)) {
-				return;
-			}
+					auto& soLoudSource = registry.get<SoLoudAudioSource>(soundResourceHandle.mResourceEntity);
 
-			const auto& soLoudSource = registry.get<SoLoudAudioSource>(soundResourceHandle.mResourceEntity);
+					auto handle = mSoloud.play(*soLoudSource.wav);
+					mSoloud.setLooping(handle, request.looping);
 
-			auto handle = mSoloud.play(*soLoudSource.wav);
-			mSoloud.setLooping(handle, request.looping);
+					registry.erase<PlaySoundSourceRequest>(entity);
+				});
+	}
 
-            registry.erase<PlaySoundSourceRequest>(entity);
-        });
-    }
-
-} // Audio
+} // namespace Audio
