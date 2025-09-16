b8cb2634d5c9a95ecd56f14561f190b39513c7af
Niamh Cuileann <sweet.niamhy@gmail.com>
2025-09-15 21:36:05 -0400

    Disabling audio lib it will need to be stubbed.

--- tests/SoLoudSystemTests.cpp
+++ tests/SoLoudSystemTests.cpp
@@ -10,20 +10,20 @@ import Audio.SoundPlayback;
 import Core.EnTTRegistry;
 import Core.Scheduler;
 
-TEST_CASE("AudioSourceWithEmptySoundAndPlaybackRequest_TickSoLoudSystem_NoPlaybackHandleAndRequestRemoved", "SoLoudSystem") {
-	using namespace Audio;
-	Core::EnTTRegistry enttRegistry;
-	Core::Scheduler scheduler;
-	SoLoudSystem soLoudSystem(enttRegistry, scheduler);
-	entt::registry& registry(enttRegistry);
-	const entt::entity audioSourceEntity{ registry.create() };
-	registry.emplace<AudioSource>(audioSourceEntity);
-	registry.emplace<PlaySoundSourceRequest>(audioSourceEntity);
-
-	scheduler.tick();
-
-	const bool hasSoundPlaybackHandle = registry.all_of<SoundPlayback>(audioSourceEntity);
-	const bool hasPlaySoundSourceRequest = registry.all_of<PlaySoundSourceRequest>(audioSourceEntity);
-	REQUIRE(!hasSoundPlaybackHandle);
-	REQUIRE(!hasPlaySoundSourceRequest);
-}
+// TEST_CASE("AudioSourceWithEmptySoundAndPlaybackRequest_TickSoLoudSystem_NoPlaybackHandleAndRequestRemoved", "SoLoudSystem") {
+// 	using namespace Audio;
+// 	Core::EnTTRegistry enttRegistry;
+// 	Core::Scheduler scheduler;
+// 	SoLoudSystem soLoudSystem(enttRegistry, scheduler);
+// 	entt::registry& registry(enttRegistry);
+// 	const entt::entity audioSourceEntity{ registry.create() };
+// 	registry.emplace<AudioSource>(audioSourceEntity);
+// 	registry.emplace<PlaySoundSourceRequest>(audioSourceEntity);
+//
+// 	scheduler.tick();
+//
+// 	const bool hasSoundPlaybackHandle = registry.all_of<SoundPlayback>(audioSourceEntity);
+// 	const bool hasPlaySoundSourceRequest = registry.all_of<PlaySoundSourceRequest>(audioSourceEntity);
+// 	REQUIRE(!hasSoundPlaybackHandle);
+// 	REQUIRE(!hasPlaySoundSourceRequest);
+// }
